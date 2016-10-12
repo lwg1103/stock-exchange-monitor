3abdb5b707092fcb65eb72c418ef84a72c4972cc
Maciej Harbuz <m.harbuz@x-coding.pl>
2016-10-12 20:39:30 +0200

    parser

--- src/AppBundle/Utils/ReportParser/Bankier/BankierReportParser.php
+++ src/AppBundle/Utils/ReportParser/Bankier/BankierReportParser.php
@@ -5,12 +5,22 @@ namespace AppBundle\Utils\ReportParser\Bankier;
 use Symfony\Component\Debug\Exception\ContextErrorException;
 use AppBundle\Utils\ReportParser\ReportParser;
 use Company\Entity\Company;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\CssSelector\CssSelectorConverter;
 
 class BankierReportParser extends ReportParser {
 
     public function parse(Company $company) {
         $this->company = $company;
         $html = $this->getData();
+        //$dom = \Phpsimpledom\str_get_html($html);
+        $cssConverter = new CssSelectorConverter();
+        $path = $cssConverter->toXPath('div.box615.boxBlue.boxTable.left > div');
+        echo $path;
+        $dom = new Crawler($html);
+        
+        $dom = $dom->filter($path);
+        echo count($dom);die;
         $data = array();
         $data['company'] = $company;
         $data['identifier'] = new \DateTime('2015-06-30');
@@ -20,13 +30,14 @@ class BankierReportParser extends ReportParser {
         $data['bookValue'] = '15';
         $data['sharesQuantity'] = '16';
         $data['assets'] = '17';
-        $report = $this->prepareReport($data);
-        $this->em->persist($report);
-        $this->em->flush();
+        //$report = $this->prepareReport($data);
+        //$this->em->persist($report);
+        //$this->em->flush();
     }
 
 
     private function getReportUrl() {
+    	return "http://www.bankier.pl/gielda/notowania/akcje/PZU"."/wyniki-finansowe";
         return "http://www.bankier.pl/gielda/notowania/akcje/".$this->company->getMarketId()."/wyniki-finansowe";
     }
 
