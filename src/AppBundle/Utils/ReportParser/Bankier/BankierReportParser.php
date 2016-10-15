dd0ad0fd64a73758f8fbc1cd44c588af3c27813f
Maciej Harbuz <m.harbuz@x-coding.pl>
2016-10-15 21:07:30 +0200

    parser

--- src/AppBundle/Utils/ReportParser/Bankier/BankierReportParser.php
+++ src/AppBundle/Utils/ReportParser/Bankier/BankierReportParser.php
@@ -17,57 +17,45 @@ class BankierReportParser extends ReportParser {
         $cssConverter = new CssSelectorConverter();
         //$path = $cssConverter->toXPath('.box615');
         $dom = new Crawler($html);
-        
-        $header = null;
-        $headers = $dom->filter('div[class="boxHeader"] > h2')
-        			   ->each(function(Crawler $node, $i) {
-        	if($node->text() == "Skonsolidowane raporty kwartalne") {
+
+        $table = $this->getHtmlTable($dom);
+
+        $availableQuarters = $this->getAvailableQuarters($table);
+
+        $reportDataTrs = $this->getReportData($table);
+
+        $reports = array();
+        foreach($reportDataTrs as $tr) {
+        	$tds = $tr->filter('td')->each(function(Crawler $node, $i) {
         		return $node;
-        	}
-        	return null;
-        });
-        
-        foreach($headers as $h) {
-        	if($h != null) {
-        		$header = $h;
-        	}
-        }
-        
-        if($header == null) {
-        	throw new Exception('nie uda³o siê pobraæ danych');
-        }
-        
-        $ths = array();
-        $table = $header->parents()->filter('div[class="boxContent boxTable"] > table');
-        $quartersHeads = $table->filter('thead th')->each(function (Crawler $node, $i) {
-        	$quarterElement = $node->filter('strong');
-        	$quarterName = '';
-        	if($quarterElement && count($quarterElement)) {
-        		$quarterName = $quarterElement->text();
-        	}
-        	return $quarterName;
-        });
-        print_r($quartersHeads);
-        $availableQuarters = array();
-        
-        for($i=0; $i < count($quartersHeads); $i++) {
-        	$re = '/[I|V]+ Q \d{4}/';
-        	$match = preg_match($re, $quartersHeads[$i], $matches);
-        	if($match) {
-        		$availableQuarters[$i] = $quartersHeads[$i];
+        	});
+
+        	$reportDataKey = '';
+        	for($i=0; $i<count($tds); $i++) {
+        		if($i==0) {
+        			$reportDataName = trim($tds[$i]->text());
+
+        			try {
+        				$reportDataKey = $this->getReportDataKey($reportDataName);
+        			} catch (\Exception $e) {
+        				continue 2;
+        			}
+        		}
+        		else {
+        			$reportDataValue = $tds[$i]->text();
+        			$reportDataValue = preg_replace('/\D/', '', $reportDataValue);
+        			$reports[$availableQuarters[$i]][$reportDataKey] = $reportDataValue;
+        		}
         	}
         }
-        //echo $table->text();
-        
-        
-        $reportData = $table->filter('tbody tr')->each(function (Crawler $node, $i) {
-        	return $node;
-        });
-        
-        print_r($reportData);
-        
+
+        print_r($reports);
+
+
+        //print_r($reportData);
+
         die;
-        
+
         $data = array();
         $data['company'] = $company;
         $data['identifier'] = new \DateTime('2015-06-30');
@@ -82,6 +70,75 @@ class BankierReportParser extends ReportParser {
         //$this->em->flush();
     }
 
+    private function getHtmlTable(Crawler $dom) {
+    	$header = null;
+    	$headers = $dom->filter('div[class="boxHeader"] > h2')
+    		->each(function(Crawler $node, $i) {
+    			if($node->text() == "Skonsolidowane raporty kwartalne") {
+	    			return $node;
+	    		}
+	    		return null;
+	    	});
+
+    	foreach($headers as $h) {
+    		if($h != null) {
+    			$header = $h;
+    		}
+    	}
+
+   		if($header == null) {
+   			throw new Exception('nie uda?o si?pobra?danych');
+   		}
+
+   		$ths = array();
+   		$table = $header->parents()->filter('div[class="boxContent boxTable"] > table');
+
+   		return $table;
+    }
+
+    private function getAvailableQuarters($table) {
+    	$quartersHeads = $table->filter('thead th')
+    		->each(function (Crawler $node, $i) {
+	    		$quarterElement = $node->filter('strong');
+	    		$quarterName = '';
+	    		if($quarterElement && count($quarterElement)) {
+	    			$quarterName = $quarterElement->text();
+	    		}
+	    		return $quarterName;
+	    	});
+
+    	$availableQuarters = array();
+
+   		for($i=0; $i < count($quartersHeads); $i++) {
+   			$re = '/[I|V]+ Q \d{4}/';
+   			$match = preg_match($re, $quartersHeads[$i], $matches);
+   			if($match) {
+   				$availableQuarters[$i] = $quartersHeads[$i];
+   			}
+   		}
+
+   		return $availableQuarters;
+    }
+
+    private function getReportData($table) {
+    	$reportData = $table->filter('tbody tr')->each(function (Crawler $node, $i) {
+    		return $node;
+    	});
+
+    	return $reportData;
+    }
+
+    private function getReportDataKey($reportDataName) {
+
+    	$translation = array(
+    		'Zysk (strata) netto (tys. zÅ‚)*' => 'netProfit',
+    	);
+    	if(!array_key_exists($reportDataName, $translation)) {
+    		throw new \Exception('nieznana dana');
+    	}
+    	return $translation[$reportDataName];
+    }
+
 
     private function getReportUrl() {
     	return "http://www.bankier.pl/gielda/notowania/akcje/PZU"."/wyniki-finansowe";
@@ -93,7 +150,7 @@ class BankierReportParser extends ReportParser {
         try {
             $html = file_get_contents($this->getReportUrl());
         } catch (ContextErrorException $e) {
-            throw new Exception('nie uda³o siê pobraæ danych');
+            throw new Exception('nie uda?o si?pobra?danych');
         }
 
         return $html;
