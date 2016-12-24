121e918d49c9a77261a9ba8f8932555aa30cf8a7
d.czajka <d.czajka@clearcode.cc>
2016-12-24 10:35:54 +0100

    (#581) added companies from wig30 to fixtures and bossa translator Fixed regexp bug causing error when company has listed bonds

--- features/DataFixtures/LoadCompanyData.php
+++ features/DataFixtures/LoadCompanyData.php
@@ -18,10 +18,8 @@ class LoadCompanyData implements OrderedFixtureInterface, FixtureInterface
     {
         $this->manager = $manager;
 
-        $this->addCompany("PKO BP", "PKO");
-        $this->addCompany("PGNiG", "PGN");
-        $this->addCompany("Elektrobudowa", "ELB");
-        $this->addCompany("Asseco Poland", "ACP");
+        $this->addWig30Companies();
+        $this->addWigBudowCompanies();
 
         $this->manager->flush();
     }
@@ -31,6 +29,45 @@ class LoadCompanyData implements OrderedFixtureInterface, FixtureInterface
         $company = new Company($name, $id);
 
         $this->manager->persist($company);
+    }
+
+    private function addWig30Companies()
+    {
+        $this->addCompany("Alior Bank", "ALR");
+        $this->addCompany("Asseco Poland", "ACP");
+        $this->addCompany("Bank Millenium", "MIL");
+        $this->addCompany("Bogdanka", "LWB");
+        $this->addCompany("BZ WBK", "BZW");
+        $this->addCompany("CCC", "CCC");
+        $this->addCompany("CD Projejkt", "CDR");
+        $this->addCompany("Cyfrowy Polsat", "CPS");
+        $this->addCompany("ENEA", "ENA");
+        $this->addCompany("Energa", "ENG");
+        $this->addCompany("Eurocash", "EUR");
+        $this->addCompany("Grupa Azoty", "ATT");
+        $this->addCompany("Grupa Lotos", "LTS");
+        $this->addCompany("GTC", "GTC");
+        $this->addCompany("ING Bank Slaski", "ING");
+        $this->addCompany("Jastrzebska Spolka Weglowa", "JSW");
+        $this->addCompany("Kernel Holding", "KER");
+        $this->addCompany("KGHM Polska Miedz", "KGH");
+        $this->addCompany("LPP", "LPP");
+        $this->addCompany("MBANK", "MBK");
+        $this->addCompany("Orange Polska", "OPL");
+        $this->addCompany("PEKAO", "PEO");
+        $this->addCompany("PGE", "PGE");
+        $this->addCompany("PGNiG", "PGN");
+        $this->addCompany("PKN Orlen", "PKN");
+        $this->addCompany("PKO BP", "PKO");
+        $this->addCompany("PKP Cargo", "PKP");
+        $this->addCompany("PZU", "PZU");
+        $this->addCompany("Synthos", "SNS");
+        $this->addCompany("Tauron Polska Energia", "TPE");
+    }
+
+    private function addWigBudowCompanies()
+    {
+        $this->addCompany("Elektrobudowa", "ELB");
     }
 
 }
--- src/Company/Translator/BossaMarketIdTranslator.php
+++ src/Company/Translator/BossaMarketIdTranslator.php
@@ -5,10 +5,37 @@ namespace Company\Translator;
 class BossaMarketIdTranslator
 {
     private $bossaMarketId = [
+        'ALR' => "ALIOR",
         'ACP' => "ASSECOPOL",
-        'ELB' => "ELBUDOWA",
+        'MIL' => "MILLENNIUM",
+        'LWB' => "BOGDANKA",
+        'BZW' => "BZWBK",
+        'CCC' => "CCC",
+        'CDR' => "CDPROJEKT",
+        'CPS' => "CYFRPLSAT",
+        'ENA' => "ENEA",
+        'ENG' => "ENERGA",
+        'EUR' => "EUROCASH",
+        'ATT' => "GRUPAAZOTY",
+        'LTS' => "LOTOS",
+        'GTC' => "GTC",
+        'ING' => "INGBSK",
+        'JSW' => "JSW",
+        'KER' => "KERNEL",
+        'KGH' => "KGHM",
+        'LPP' => "LPP",
+        'MBK' => "MBANK",
+        'OPL' => "ORANGEPL",
+        'PEO' => "PEKAO",
+        'PGE' => "PGE",
         'PGN' => "PGNIG",
-        'PKO' => "PKOBP"
+        'PKN' => "PKNORLEN",
+        'PKO' => "PKOBP",
+        'PKP' => "PKPCARGO",
+        'PZU' => "PZU",
+        'SNS' => "SYNTHOS",
+        'TPE' => "TAURONPE",
+        'ELB' => "ELBUDOWA",
     ];
 
     public function translateFromMarketId($marketId)
