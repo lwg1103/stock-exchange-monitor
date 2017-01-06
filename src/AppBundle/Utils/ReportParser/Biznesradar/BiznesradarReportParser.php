4ad5475efaef80bb3d8739e37db6b7dfe47849c2
Maciej Harbuz <m.harbuz@x-coding.pl>
2017-01-06 08:52:57 +0100

    more than just netprofit

--- src/AppBundle/Utils/ReportParser/Biznesradar/BiznesradarReportParser.php
+++ src/AppBundle/Utils/ReportParser/Biznesradar/BiznesradarReportParser.php
@@ -9,45 +9,26 @@ use Symfony\Component\DomCrawler\Crawler;
 use Symfony\Component\CssSelector\CssSelectorConverter;
 
 class BiznesradarReportParser extends ReportParser {
+	
+	var $reports = array();
+	var $availableReports = array();
 
     public function parse(Company $company) {
         $this->company = $company;
-        $html = $this->getData();
-        //$dom = \Phpsimpledom\str_get_html($html);
-        $cssConverter = new CssSelectorConverter();
-        //$path = $cssConverter->toXPath('.box615');
-        $dom = new Crawler($html);
-
-        $table = $this->getHtmlTable($dom);
-
-        $availableReports = $this->getAvailableReports($table);
-
-        $reportDataTrs = $this->getReportData($table);
         
-
-        $reports = array();
-        foreach($reportDataTrs as $tr) {
-        	$tds = $tr->filter('td')->each(function(Crawler $node, $i) {
-        		return $node;
-        	});
-        	try {
-        		$reportDataKey = $this->getReportDataKey($tr->attr("data-field"));
-        	} catch (\Exception $e) {
-        		continue;
-        	}
-        	
-        	for($i=0; $i<count($tds); $i++) {
-        		if(count($availableReports) <= $i) {
-        			continue;
-        		}
-        		$reportDataValue = $tds[$i]->text();
-        		$reportDataValue = preg_replace('/\D/', '', $reportDataValue);
-        		$reports[$availableReports[$i]][$reportDataKey] = $reportDataValue;
-        		
-        	}
+        /*$urlRZIS = $this->getReportRZISUrl();
+        $urlBilans = $this->getReportBilansUrl();
+        $urlWR = $this->getReportWRUrl();*/
+        $urls = array();
+        //$urls[] = $this->getReportWRUrl();
+        //$urls[] = $this->getReportBilansUrl();
+        $urls[] = $this->getReportRZISUrl();
+        
+        foreach($urls as $url) {
+        	$this->parsePage($url);
         }
-
-        print_r($reports);
+        	
+        print_r($this->reports);
         //print_r($reportData);
 
         die;
@@ -61,9 +42,66 @@ class BiznesradarReportParser extends ReportParser {
         $data['bookValue'] = '15';
         $data['sharesQuantity'] = '16';
         $data['assets'] = '17';
+        $data['currentAssets'] = '17';
+        $data['liabilities'] = '17';
+        $data['currentLiabilities'] = '17';
         //$report = $this->prepareReport($data);
         //$this->em->persist($report);
         //$this->em->flush();
+    }
+    
+    private function parsePage($url) {
+    	$html = $this->getData($url);
+    	//$dom = \Phpsimpledom\str_get_html($html);
+    	$cssConverter = new CssSelectorConverter();
+    	//$path = $cssConverter->toXPath('.box615');
+    	$dom = new Crawler($html);
+    	
+    	$table = $this->getHtmlTable($dom);
+    	
+    	$this->availableReports = $this->getAvailableReports($table);
+    	
+    	$reportDataTrs = $this->getReportData($table);
+    	
+    	foreach($reportDataTrs as $tr) {
+    		try {
+    			$this->parseRow($tr);
+    		} 
+    		catch (\Exception $e) {
+    			echo 'e';
+    			continue;
+    		}
+    	}
+    }
+    
+    private function parseRow($tr) {
+    	$tds = $tr->filter('td[class="h"]')->each(function(Crawler $node, $i) {
+    		return $node;
+    	});
+    	
+    	$reportDataKey = $this->getReportDataKey($tr->attr("data-field"));
+    	
+    	for($i=0; $i<count($tds); $i++) {
+    		if(count($this->availableReports) <= $i) {
+    			continue;
+    		}
+    		echo 'i';
+    		$reportDataValue = $tds[$i]->filter('span[class="value"] > span[class="pv"]')
+	    		->each(function (Crawler $node, $i) {
+	    			$value = $node->text();
+	    			return $value;
+	    		});
+    		//$reportDataValue = $reportDataValue->text();
+    		var_dump($reportDataValue);
+    		if(count($reportDataValue)) {
+    			$reportDataValue = preg_replace('/\D/', '', $reportDataValue[0]);
+    		}
+    		else {
+    			$reportDataValue = 0;
+    		}
+    		 
+    		$this->reports[$this->availableReports[$i]][$reportDataKey] = $reportDataValue;
+    	}
     }
 
     private function getHtmlTable(Crawler $dom) {
@@ -106,22 +144,41 @@ class BiznesradarReportParser extends ReportParser {
     	
     	$translation = array(
     		'IncomeNetProfit' => 'netProfit',
+    		'BalanceCurrentAssets' => 'currentAssets',
+    		'BalanceTotalAssets' => 'assets',
+    		'BalanceCurrentLiabilities' => 'currentLiabilities',
+    		'BalanceTotalLiabilities' => 'liabilities',
+    		'ShareAmountCurrent' => 'sharesQuantity',
+    		'WKCurrent' => 'bookValue_per_share',
+    		'IncomeEBIT' => 'operationalNetProfit',
+    		'IncomeRevenues' => 'income_part1',
+    		'IncomeOtherOperatingIncome' => 'income_part2',
+    		'IncomeFinanceIncome' => 'income_part3'
     	);
+    	
     	if(!array_key_exists($reportDataName, $translation)) {
     		throw new \Exception('nieznana dana');
     	}
+    	
     	return $translation[$reportDataName];
     }
 
-    private function getReportUrl() {
-    	return "http://www.biznesradar.pl/raporty-finansowe-rachunek-zyskow-i-strat/PKO";
-        return "http://www.biznesradar.pl/raporty-finansowe-rachunek-zyskow-i-strat/".$this->company->getMarketId();
+    private function getReportRZISUrl() {
+		return "http://www.biznesradar.pl/raporty-finansowe-rachunek-zyskow-i-strat/".$this->company->getMarketId();
     }
-
-    private function getData() {
+    
+    private function getReportBilansUrl() {
+    	return "http://www.biznesradar.pl/raporty-finansowe-bilans/".$this->company->getMarketId();
+    }
+    
+    private function getReportWRUrl() {
+    	return "http://www.biznesradar.pl/wskazniki-wartosci-rynkowej/".$this->company->getMarketId();
+    }
+    
+    private function getData($url) {
 
         try {
-            $html = file_get_contents($this->getReportUrl());
+            $html = file_get_contents($url);
         } catch (ContextErrorException $e) {
             throw new Exception('nie udało się pobrać danych');
         }
