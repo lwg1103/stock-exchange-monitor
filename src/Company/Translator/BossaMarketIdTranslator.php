ab93e2766cf46b58f3cc427714a12714ec340acc
Maciej Harbuz <m.harbuz@x-coding.pl>
2018-02-15 21:20:11 +0100

    BossaTranslator change - inline table to long_market_id from company table

--- src/Company/Translator/BossaMarketIdTranslator.php
+++ src/Company/Translator/BossaMarketIdTranslator.php
@@ -2,59 +2,40 @@
 
 namespace Company\Translator;
 
+use Doctrine\ORM\EntityRepository;
+
 class BossaMarketIdTranslator
 {
-    private $bossaMarketId = [
-        'ALR' => "ALIOR",
-        'ACP' => "ASSECOPOL",
-        'MIL' => "MILLENNIUM",
-        'LWB' => "BOGDANKA",
-        'BZW' => "BZWBK",
-        'CCC' => "CCC",
-        'CDR' => "CDPROJEKT",
-        'CPS' => "CYFRPLSAT",
-        'ENA' => "ENEA",
-        'ENG' => "ENERGA",
-        'EUR' => "EUROCASH",
-        'ATT' => "GRUPAAZOTY",
-        'LTS' => "LOTOS",
-        'GTC' => "GTC",
-        'ING' => "INGBSK",
-        'JSW' => "JSW",
-        'KER' => "KERNEL",
-        'KGH' => "KGHM",
-        'LPP' => "LPP",
-        'MBK' => "MBANK",
-        'OPL' => "ORANGEPL",
-        'PEO' => "PEKAO",
-        'PGE' => "PGE",
-        'PGN' => "PGNIG",
-        'PKN' => "PKNORLEN",
-        'PKO' => "PKOBP",
-        'PKP' => "PKPCARGO",
-        'PZU' => "PZU",
-        'SNS' => "SYNTHOS",
-        'TPE' => "TAURONPE",
-        'ELB' => "ELBUDOWA",
-    ];
+    /** @var EntityRepository */
+    private $companyRepository;
+    
+    /**
+     * BossaMarketIdTranslator constructor.
+     * @param EntityRepository $companyRepository
+     */
+    public function __construct(EntityRepository $companyRepository)
+    {
+        $this->companyRepository = $companyRepository;
+    }
+    
 
     public function translateFromMarketId($marketId)
     {
-        if (!array_key_exists($marketId, $this->bossaMarketId)) {
+        $company = $this->companyRepository->findOneBy(['marketId' => $marketId]);
+        if(!$company || !$company->getLongMarketId()) {
             throw new TranslatorException(sprintf("Missing BossaId for %s", $marketId));
         }
-
-        return $this->bossaMarketId[$marketId];
+        
+        return $company->getLongMarketId();
     }
     
     public function translateToMarketId($bossaMarketId)
     {
-        $key = array_search($bossaMarketId, $this->bossaMarketId);
-
-        if (!$key) {
-            throw new TranslatorException(sprintf("Missing MarketId for %s", $bossaMarketId));
+        $company = $this->companyRepository->findOneBy(['longMarketId' => $bossaMarketId]);
+        if(!$company) {
+            throw new TranslatorException(sprintf("Missing company for bossaId (long_market_id) %s", $bossaMarketId));
         }
-
-        return $key;
+        
+        return $company->getMarketId();
     }
 }
